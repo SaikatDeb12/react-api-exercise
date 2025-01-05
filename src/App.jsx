1850bf4b4802ab5b112365f8de8809a97f34d251
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-05 17:02:52 +0530

    generating an excuse

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,52 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  // useEffect(() => {}, []);
+  const [excuse, setValue] = useState("");
+
+  return (
+    <div>
+      <h1>Generate an Excuse</h1>
+      <button
+        onClick={() => {
+          axios
+            .get(`https://excuser-three.vercel.app/v1/excuse/party/`)
+            .then((event) => {
+              setValue(event.data[0].excuse);
+              // console.log(event.data[0].excuse);
+            });
+        }}
+      >
+        Party
+      </button>
+      <button
+        onClick={() => {
+          axios
+            .get(`https://excuser-three.vercel.app/v1/excuse/family/`)
+            .then((event) => {
+              setValue(event.data[0].excuse);
+              // console.log(event.data);
+            });
+        }}
+      >
+        Family
+      </button>
+      <button
+        onClick={() => {
+          axios
+            .get(`https://excuser-three.vercel.app/v1/excuse/office/`)
+            .then((event) => {
+              setValue(event.data[0].excuse);
+              // console.log(event.data);
+            });
+        }}
+      >
+        Office
+      </button>
+      <h2>Excuse: {excuse}</h2>
+    </div>
+  );
+
+  /*
   const [data, setData] = useState({});
   const [user, setName] = useState("");
   return (
@@ -27,6 +72,7 @@ function App() {
       <h2>Predicted age: {data.age}</h2>
     </div>
   );
+  */
 }
 
 export default App;
